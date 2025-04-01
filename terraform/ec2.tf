bf538d0e2453a6f05be1e52077eab53c13e767a3
Adnan07 <adnankhan0704@gmail.com>
2025-04-01 17:36:34 +0530

    Production-level

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,6 +1,8 @@
 resource "aws_key_pair" "deployer" {
   key_name   = "terra-automate-key"
-  public_key = file("/Users/shubham/Documents/work/TrainWithShubham/terra-practice/terra-key.pub")
+  public_key = file("C:\\Users\\hp\\Downloads\\Devops\\CICD Project\\Wanderlust-Mega-Project\\terraform\\terra-key.pub")
+
+
 }
 
 resource "aws_default_vpc" "default" {
